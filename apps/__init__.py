23fcbb1ced5c6358f92621a0545a5b03881e5226
JantharatChumsang <jantharatchjune@gmail.com>
2023-03-14 12:47:58 +0700

    first commit

---
+++ apps/__init__.py
@@ -0,0 +1,7 @@
+from apps.home_app import HomeApp
+from apps.member_app import MemberApp
+from apps.pred_app import PredictApp
+from apps.Howto_app import HowtoApp
+from apps.intro_app import IntroApp
+from apps.contact_us import ContactUsAPP
+from apps.dashb_app import DashbApp
