92b4083723a66781f7065b3668eb5abb5a003cdd
Jantharat Chumsang <jantharatchjune@gmail.com>
2023-03-20 15:55:11 +0700

    Update home_app.py

--- apps/home_app.py
+++ apps/home_app.py
@@ -38,7 +38,9 @@ class HomeApp(HydraHeadApp):
                     #st.text("Welcome to✨")
                     st.title("wAAPP: Web Application for Antimicrobial Peptide Prediction ")
                     #st.subheader("เว็บแอปพลิเคชันสำหรับการทำนายเพปไทด์ต้านจุลชีพ")
-                    Ideal_title = '<p style="font-family:; color:#31333F; font-size: 20px; ">wAAPP: Web Application to test the antimicrobial peptide activity against bacteria.</p>'
+                    Ideal_title = '<p style="font-family:; color:#31333F; font-size: 20px; ">WAAPP:</p>'
+                    # st.markdown(Ideal_title, unsafe_allow_html=True)
+                    Ideal_title = '<p style="font-family:; color:#31333F; font-size: 20px; ">Web Application to test the antimicrobial peptide activity against bacteria.</p>'
                     # st.markdown(Ideal_title, unsafe_allow_html=True)
                     # st.write("##")
                     Ideal_title = '<p style="font-family:; color:#06BBCC; font-size: 18px; ">Click to see photo credits.</p>'
